70ea420c5b1bc673b224efd66e3102504a6abe7f
sushain garg <gargsushain@gmail.com>
2024-01-11 02:37:30 +0000

    path change for OAuth component

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom"
-import OAuth from "../components/oauth";
+import OAuth from "../components/OAuth";
 
 export default function Signup() {
   const [formData , setFormData] = useState({})
